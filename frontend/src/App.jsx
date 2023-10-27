25dd820c6a311ed59810729007b05cf8e7bd745f
MAbdurahman <mdbdrrhm@gmail.com>
2023-10-27 08:52:44 -0500

    Create pages/, components/, and basic structure for routes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Header from './components/Header.jsx';
+import About from './pages/About.jsx';
+import Profile from './pages/Profile.jsx';
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+
 export default function App() {
 
     return (
-        <h1 className="text-red-500 font-bold underline">
-            App
-        </h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<Home />} />
+                <Route path={"/sign-in"} element={<SignIn />} />
+                <Route path={"/sign-up"} element={<SignUp />} />
+                <Route path={"/about"} element={<About />} />
+                <Route path={"/profile"} element={<Profile />} />
+                <Route path={"*"} element={<PageNotFound />} />
+            </Routes>
+        </BrowserRouter>
 
     );
 }
---
+++ frontend/src/pages/PageNotFound.jsx
@@ -0,0 +1,9 @@
+export default function PageNotFound() {
+
+    return (
+        <div>
+            <h2>PageNotFound</h2>
+        </div>
+
+    );
+}
