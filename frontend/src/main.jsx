01f9c05aa5d7aa52888bd7c3269fb16742717a03
MAbdurahman <mdbdrrhm@gmail.com>
2023-10-28 14:52:21 -0500

    Add redux persist to the project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,10 @@ import App from './App.jsx';
 import './index.css';
 import {persistor, store} from './redux/store.js';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-    <App/>
-    </Provider>);
+ReactDOM.createRoot(document.getElementById('root')).render(<Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+        <App/>
+    </PersistGate>
+</Provider>);
