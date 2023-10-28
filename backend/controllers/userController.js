7f4d3da8f7094a8982fc3fa18bc4e728d2c6af94
MAbdurahman <mdbdrrhm@gmail.com>
2023-10-28 06:41:26 -0500

    Modify: message in backend/controllers/userController.js and frontend/src/pages/SignUp.jsx

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,6 @@
 
 export const test = (req, res) => {
     res.json({
-        message: 'This is API Home for mern-real-estate is working.'
+        message: 'API Home for mern-real-estate is working.'
     })
 }
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import {Link, useNavigate} from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 
 export default function SignUp() {
     const [formData, setFormData] = useState({});
@@ -75,7 +75,7 @@ export default function SignUp() {
 
             </form>
             <div className='flex gap-2 mt-5'>
-                <p>Have an account?</p>
+                <p>Already have an account?</p>
                 <Link to={'/sign-in'}>
                     <span className='text-blue-700'>Sign in</span>
                 </Link>
