be9da7055650b8ff390cc9b226f51fee20a46252
MAbdurahman <mdbdrrhm@gmail.com>
2023-11-07 08:41:23 -0600

    Temporary structure for pages/PageNotFound.jsx

--- backend/server.js
+++ backend/server.js
@@ -12,15 +12,15 @@ import userRouter from './routes/userRoute.js';
 import authRouter from './routes/authRoute.js';
 import listingRouter from './routes/listingRoute.js'
 
+//**************** configuration setup ****************//
+dotenv.config({path: 'backend/config/config.env'});
+colors.enable();
+
 //**************** variables ****************//
 const app = express();
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV;
-const API = process.env.API_ENV || "/api/v1.0/";
-
-//**************** configuration setup ****************//
-dotenv.config({path: 'backend/config/config.env'});
-colors.enable();
+const API_URL = process.env.API_ENV || "/api/v1.0/";
 
 //**************** connect to database ****************//
 connectDatabase();
@@ -34,7 +34,7 @@ app.use(cookieParser());
 
 //**************** app listening ****************//
 const server = app.listen(PORT, () => {
-    console.log(`The server is listening at - http://127.0.0.1:${PORT}${API} in ${NODE_ENV} mode🔥`.yellow);
+    console.log(`The server is listening at - http://127.0.0.1:${PORT}${API_URL} in ${NODE_ENV} mode🔥`.yellow);
 });
 
 //**************** routes****************//
--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -1,9 +1,22 @@
+import {Link} from 'react-router-dom';
+
 export default function PageNotFound() {
 
-    return (
-        <div>
-            <h2>PageNotFound</h2>
-        </div>
+    return (<>
+            <div
+                className="h-screen w-screen bg-blue-600 flex flex-col text-center justify-center content-center flex-wrap">
+                <p className="font-sans text-9xl text-white error-text">404</p>
+                <p className="font-sans text-5xl text-white">Page Not Found!</p>
+                <p className="font-sans text-4xl text-white capitalize">back to</p>
+                <Link
+                    to={'/'}
+                    className="bg-transparent text-white hover:bg-blue-300 outline-2 font-semibold text-lg uppercase text-center font-sans"
+                >
+
+                    home
+                </Link>
+            </div>
+        </>
 
     );
 }
